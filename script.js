c44c492602cac2704db91302452aa4c6d56ae6a2
Wackeroni <167975245+Wackeroni@users.noreply.github.com>
2025-02-14 01:33:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,431 @@
+const projects = [
+    {
+        id: '01',
+        title: "Ridin': Cycle Community Hub",
+        description: `This was the project I was most proud of. Introducing,
+Ridin’: the cycle community hub. With the goal of
+sustainable cities and communities, i targeted 11-6, to
+reduce the environmental impact of Singapore.
+I wanted to meet the needs of the cycling community as
+they are a target audience we don’t usually cater to.
+There have been road accidents due to cyclist being
+careless, which reduced public’s interest in wanting to
+take up cycling. 
+
+I have proposed my space to meet their
+needs and at the same time, celebrate the sport of
+cycling and make it seen to all.
+I had a lot of fun for my final project and it was very
+memorable. With the skills i gained during internship, I
+managed to carry that on for my final project. This is a
+project i will treasure for a long time.`,
+        images: [
+            'images/project_one_image1.png',
+            'images/project_one_image2.png',
+            'images/project_one_image3.png',
+            'images/project_one_image4.png',
+            'images/project_one_image5.png',
+            'images/project_one_image6.png',
+            'images/project_one_image7.png'
+        ]
+    },
+    {
+        id: '02',
+        title: 'Grid',
+        description: `I was exploring form in sketchup for architectural
+purposes. With the numerous amount for green spaces
+and voids, it is naturally ventilated and really
+sustainable.
+i wanted to test the skills i gained from my internship
+and explored by creating a grid building in sketchup. i
+love to explore during my free time and see how far i
+can go, thus i came up with this creation.`,
+        images: [
+            'images/project_two_image1.png',
+            'images/project_two_image2.png',
+            'images/project_two_image3.png',
+            'images/project_two_image4.png',
+            'images/project_two_image5.png',
+        ]
+    },
+    {
+        id: '03',
+        title: 'Zen',
+        description: `During my first internship programme, I managed to
+polish my skills in sketchup and learnt a rendering
+software called Enscape. During my free time, I tested
+my skills out and modelled an office space from scratch.
+this exploration was not only to test my skills, but to
+also understand design principals in an office space.
+in this space, I integrated the outdoors and indoors. this
+was to induce the phycology of introducing nature
+within our spaces to improve mental health and
+wellbeing of people at the workplace. additionally, i
+designed using warm tones, Zen atmosphere and a
+biophilic working environment.
+Though this space was just to experiment, it helped me
+understood the importance of how a workplace should
+be design and taking note of a person’s wellbeing in an
+office space as well. phycological needs have to be met
+in order for a spiritual, vibrant and peaceful mindset.`,
+        images: [
+            'images/project_three_image1.png',
+            'images/project_three_image2.png',
+            'images/project_three_image3.png',
+            'images/project_three_image4.png',
+            'images/project_three_image5.png',
+            'images/project_three_image6.png',
+            'images/project_three_image7.png',
+            'images/project_three_image8.png',
+            'images/project_three_image9.png',
+        ]
+    },
+    {
+        id: '04',
+        title: 'Pantry Design',
+        description: `This was another project i did during my free time in
+        internship. Since my company focuses on office spaces, i
+        decided to practice pantry designs. i came up with this
+        design to practice my Sketchup and Enscape skills.
+        working on this small area, i realised how fast i
+        modelled this space in sketchup. This software is very
+        versatile and 3d warehouse made it very easy to install
+        furnitures and lightings. i also practised rendering in
+        enscape for details like the light flare and illumination.
+        the light fixtures on the open ceiling gave an illusion
+        that its lit and bright. i achieved that through
+        illuminating material and adjusting visual settings in
+        Enscape`
+        ,
+        images: [
+            'images/project_four_image1.png',
+            'images/project_four_image2.png',
+            'images/project_four_image3.png',
+        ]
+    },
+    {
+        id: '05',
+        title: 'Bar',
+        description: `This was an assignment assigned by my lecturer to
+explore 3d max features and create an interior, inspired
+by a real-life project. I got inspired by an unknown bar I
+found on ArchDaily, and decided to model it up and
+tweak the design.
+I love biophilic and nature inspired buildings. I try to
+input a lot of biophilic structures and elements into my
+designs. I experimented with lightings and adjusted
+certain materials and furnitures accordingly. I was
+satisfied with my design.
+Personally, I find that 3d max takes really long in
+producing a full render. If time was limited, it would be
+difficult to work with 3D max.`,
+        images: [
+            'images/project_five_image1.png',
+            'images/project_five_image2.png',
+            'images/project_five_image3.png'
+        ]
+    },
+    {
+        id: '06',
+        title: '"AADN" Dance Studio',
+        description: `This was a project i took on to help my cousin renovate
+his very own dance studio. i asked my cousin how he
+envisioned his future studio to be like and he gave me a
+list of what he wanted in it. A green screen studio,
+person open space office and vinyl floor dance area.
+i began modelling out his site, and began to strategise
+on where i should place the specific areas he mentioned.
+i also added a pantry for his students to grab a drink or
+bite after a tiring dance session.
+This design was a modern take on an integrated dance
+and green screen studio. with a black open ceiling
+fixture, a mix of wood, vinyl and carpet flooring to
+seperate the spaces accordingly. The personal open
+space office has a foldable, modular wall that achieves
+the personal and yet open feel of the office. i also
+designed a moss logo wall that proudly showcases his
+company’s name and a comfortable seating area.
+Overal, my cousin was very satisfied with the design and
+has considered to go about my design.`,
+        images: [
+            'images/project_six_image1.png',
+            'images/project_six_image2.png',
+            'images/project_six_image3.png',
+            'images/project_six_image4.png',
+            'images/project_six_image5.png',
+            'images/project_six_image6.png',
+        ]
+    },
+    {
+        id: '07',
+        title: 'Custom Room Set-Up',
+        description: `This was another side project i took to help one of my
+friends visualise his design. He was moving house and it
+underwent renovation. for his room, he drew out his
+custom set up and showed it to me. As he had no idea
+how to visualise it in his room, i decided to step in and
+help him out.
+I first modelled his room interior in sketchup and then
+modelled his set up. It was an integrated sliding door
+closet with a table that consist of drawers. on top of it
+were a tv and floating shelves attached to the wall.
+beside it is an integrated dressing cabinet with a mirror
+attached to the wall. My friend requested that the
+material should all be plywood. I followed his
+instructions and helped to visualise his set up in his
+room by doing up some renders.
+i went beyond and decided to do construction drawings
+as well. this was also to test my skills in Autocad and
+the ability in making quality construction and detail
+drawings.`,
+        images: [
+            'images/project_seven_image1.png',
+            'images/project_seven_image2.png',
+            'images/project_seven_image3.png',
+            'images/project_seven_image4.png'
+        ]
+    },
+    {
+        id: '08',
+        title: 'Facade',
+        description: `This was a self exploration assignment assigned by my
+lecturer to explore the Revit features and to make a
+facade. here I tried to experiment with a curve like
+structure surrounding the facade and added a curve
+glass parametric design as the roof. it was to explore the
+shadows and sunlight for the interior, hence the glass
+roof.
+Personally, I found Revit as a software to make
+structured buildings. I find it useful for making
+construction details however it limits creativity and
+room to go beyond. Creating the interior was another
+tedious job as downloading furnitures take a lot of time.
+hence, i came to a conclusion that Revit is useful for
+facade purposes but not interior purposes.`,
+        images: [
+            'images/project_eight_image1.png',
+            'images/project_eight_image2.png',
+            'images/project_eight_image3.png',
+            'images/project_eight_image4.png',
+            'images/project_eight_image5.png',
+            'images/project_eight_image6.png',
+        ]
+    },
+    {
+        id: '09',
+        title: 'Trifecta Cafe',
+        description: `This was my first intergrated studio project. the client
+was scape orhcard and we had to design an area for
+youths. Since scape was in a bad condition for a long
+time, temasek polytechnic IAD students partnered with
+scape to create a space for youths.
+With little background on architecture and interior
+design, I experimented the elevated form of cubes in
+sketchup and made it into a structure. This space
+includes a cafe with elevated platforms to go up the
+structure where level 2 includes seating areas with
+whiteboards for youths to draw and write anything they
+want.
+It also includes a space where users build furnitures out
+of building blocks for seating. this was to induce
+creativity and freedom in my structure. As users go up
+to level 3, it is a grass area where users can have picnics
+and enjoy the view way up. This whole space was to test
+my creativity and storyboarding skills for a professional
+presentation. i enjoyed the first project i got but i wished
+to had better software and rendering skills to showcase
+my work properly at this time.
+`,
+        images: [
+            'images/project_nine_image1.png',
+            'images/project_nine_image2.png',
+            'images/project_nine_image3.png',
+            'images/project_nine_image4.png',
+            'images/project_nine_image5.png',
+            'images/project_nine_image6.png',
+            'images/project_nine_image7.png',
+        ]
+    },
+    {
+        id: '10',
+        title: 'Terrace View',
+        description: `This project was my second integrated studio project.
+we collaborated with Apex Lodge nursing home and
+designed a nursing home for patients with dementia. In
+this project, we had to do reports on dementia and
+really understand the users before designing the space.
+my approach was inspired by the concept of paddy
+fields to imitate the kampung background knowledge
+for dementia patients.
+Each level had terraces of green fields where patients
+can step out and enjoy the sun. I wanted to break the
+boundaries of outdoor and indoor, and integrated the
+two into this structure. the cascading structures gave
+patients oppourtunity to step out and enjoy being
+outdoors rather than trapped indoors. My strategy was
+to also make it a loop so that dementia patients can
+walk around but within the perimeters of the home. this
+was to give them empowerment of their home and
+freedom to roam. I used Revit to model my structure
+and photoshop to render my plans`,
+        images: [
+            'images/project_ten_image1.png',
+            'images/project_ten_image2.png',
+            'images/project_ten_image3.png',
+            'images/project_ten_image4.png',
+            'images/project_ten_image5.png'
+        ]
+    },
+    {
+        id: '11',
+        title: 'Concept Models',
+        description: `Concept models were part of my design process, to
+understand form, light and shadow. I experimented and
+went all out in models as creativity was not limited. the
+following physical models were concept models to assist
+my structure’s abstract form. As for the porous
+shadows, it was created by a moss patch that i found in
+an art store and it gave an interesting atmosphere and
+feel into the model.
+i used different coloured plastics to mimic coloured
+stained glass and shone light onto it to create colourful
+shadows. it was a fun play between light and shadows.
+These models shown were concept models inspired from
+the pores and negative spaces between tree branches.
+inspired by how it gives an abstract form and how each
+negative space is a different shape, I built these concept
+models to further emphasise on it.
+`,
+        images: [
+            'images/project_eleven_image1.png',
+            'images/project_eleven_image2.png',
+            'images/project_eleven_image3.png',
+            'images/project_eleven_image4.png',
+            'images/project_eleven_image5.png',
+            'images/project_eleven_image6.png'
+        ]
+    },
+    {
+        id: '12',
+        title: 'Hand Drawings',
+        description: `In my first year in this course, we had to take lessons on
+practising our hand drawing skills. Here i wanted to
+show that i could use a variety of media to portray
+drawings. I have used pen, pencil, colour pencils and
+markers.
+The ability to mix media and use a variety of drawing
+media was to showcase that, I can portray my ideas in
+any form of media and it would still be understandable.
+In this fast pace technological world, technology and AI
+will soon take over and start to do these jobs for us.
+However, if we had a situation where we had to draw on
+the spot and showcase an idea we have to the client, i
+can use my hand drawings skills, pick up any form of
+media, and sketch my idea out swiftly, Hence, i find it
+useful that I have this skill as a designer.`,
+        images: [
+            'images/project_twelve_image1.png',
+            'images/project_twelve_image2.png',
+            'images/project_twelve_image3.png',
+        ]
+    }
+];
+
+// Create the modal elements
+const modalOverlay = document.createElement('div');
+modalOverlay.className = 'modal-overlay';
+modalOverlay.innerHTML = `
+    <div class="modal-content">
+        <button class="modal-close">×</button>
+        <img class="modal-image" src="" alt="Full screen image">
+    </div>
+`;
+document.body.appendChild(modalOverlay);
+
+const modal = {
+    overlay: modalOverlay,
+    image: modalOverlay.querySelector('.modal-image'),
+    closeBtn: modalOverlay.querySelector('.modal-close')
+};
+
+// Close modal function
+const closeModal = () => {
+    modal.overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+};
+
+// Event listeners for modal
+modal.closeBtn.addEventListener('click', closeModal);
+modal.overlay.addEventListener('click', (e) => {
+    if (e.target === modal.overlay) {
+        closeModal();
+    }
+});
+
+// Escape key to close modal
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modal.overlay.classList.contains('active')) {
+        closeModal();
+    }
+});
+
+const projectsGrid = document.getElementById('projectsGrid');
+let currentlyExpanded = null;
+
+projects.forEach(project => {
+    const projectCard = document.createElement('div');
+    projectCard.className = 'project-card';
+    projectCard.innerHTML = `
+        <div class="project-number">${project.id}</div>
+        <h3 class="project-title">${project.title}</h3>
+        <div class="project-content">
+            <p class="project-description">${project.description}</p>
+            <div class="project-gallery">
+                ${project.images.map(img => `<img src="${img}" alt="Project image">`).join('')}
+            </div>
+        </div>
+    `;
+
+    projectCard.addEventListener('click', (e) => {
+        if (e.target.tagName === 'IMG') {
+            // If clicking an image, show it in modal
+            e.stopPropagation(); // Prevent card expansion when clicking image
+            modal.image.src = e.target.src;
+            modal.overlay.classList.add('active');
+            document.body.style.overflow = 'hidden'; // Prevent scrolling when modal is open
+        } else {
+            // Regular card expansion
+            if (currentlyExpanded && currentlyExpanded !== projectCard) {
+                currentlyExpanded.classList.remove('expanded');
+            }
+            projectCard.classList.toggle('expanded');
+            currentlyExpanded = projectCard.classList.contains('expanded') ? projectCard : null;
+        }
+    });
+
+    projectsGrid.appendChild(projectCard);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const quoteContainer = document.querySelector('.quote-container');
+    
+    function checkQuote() {
+        if (!quoteContainer) return; // Safety check
+        
+        const triggerBottom = window.innerHeight * 0.8;
+        const quoteTop = quoteContainer.getBoundingClientRect().top;
+
+        if (quoteTop < triggerBottom) {
+            quoteContainer.classList.add('visible');
+        } else {
+            quoteContainer.classList.remove('visible');
+        }
+    }
+
+    // Initial check
+    checkQuote();
+
+    // Add scroll listener
+    window.addEventListener('scroll', checkQuote);
+    window.addEventListener('resize', checkQuote);
+});
